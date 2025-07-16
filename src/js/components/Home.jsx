064154bebbc3b759e40394eba58545ed8362e720
Diego Undreiner <diegoundreiner@gmail.com>
2025-07-16 00:00:47 -0400

    Finished traffic light

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [activeLight, setActiveLight] = useState(null); 
+	
+		const handleLightClick = (color) => {
+			setActiveLight(color);
+		};
+	
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<>
+		<div className="semaforo p-3">
+			<div className="hanger mx-auto"> </div>
+			<div className="square justify-content-center mx-auto py-2 d-flex align-items-center">
+				<div className={`light red ${activeLight === 'red' ? 'shadow-active' : ''}`} onClick={() => handleLightClick('red')}> </div>
+				<div className={`light yellow ${activeLight ==='yellow' ? 'shadow-active' : ''}`} onClick={() => handleLightClick('yellow')}></div>
+				<div className={`light green ${activeLight ==='green' ? 'shadow-active':''}`} onClick={() => handleLightClick('green')}></div>
+					
+			</div>
 		</div>
+		</>
 	);
 };
 
